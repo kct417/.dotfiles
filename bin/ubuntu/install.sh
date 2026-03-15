59501f78d5c73c40fe7071586f63f4e48145cb89
Casey Tran <ctran156@calpoly.edu>
2026-03-15 14:40:02 +0000

    Update cron scripts and nvim keymaps

--- bin/ubuntu/install.sh
+++ bin/ubuntu/install.sh
@@ -6,9 +6,12 @@ DEFAULT=/etc/skel/.bashrc
 BASHRC=stow/ubuntu/.bashrc
 if [ ! -f ~/.bashrc ]; then	if [ ! -f ~/.bashrc ]; then cp $DEFAULT $BASHRC; fi fi
 
-LOG=~/.cron.log
-CRON="0 12 1 * * yes | \$(which trash-empty) 30 >> $LOG"
-if ! crontab -l 2>/dev/null | grep -Fxq "$CRON"; then
-    (crontab -l 2>/dev/null; echo "$CRON") | crontab -
-	echo "Cron job installed: $CRON"
+CRON=stow/cron/.config/cron
+if [ ! -d $CRON ]; then mkdir -p $CRON; fi
+
+LOG=~/.config/cron/cron.log
+CRONJOB="0 12 1 * * yes | \$(which trash-empty) 30 >> $LOG"
+if ! crontab -l 2>/dev/null | grep -Fxq "$CRONJOB"; then
+    (crontab -l 2>/dev/null; echo "$CRONJOB") | crontab -
+	echo "Cronjob installed: $CRONJOB"
 fi
--- stow/nvim/.config/nvim/lua/config/keymaps.lua
+++ stow/nvim/.config/nvim/lua/config/keymaps.lua
@@ -3,6 +3,10 @@ local opts = { noremap = true }
 
 keymap('n', '<leader><leader>', vim.cmd.Ex, opts)
 keymap('n', '<leader>x', vim.cmd.so, opts)
+
+keymap('i', 'jk', '<ESC>', opts)
+keymap('i', 'kj', '<ESC>', opts)
+
 keymap('n', '<leader>u', ':up<CR>', opts)
 keymap('n', '<leader>w', ':w<CR>', opts)
 keymap('n', '<leader>q', ':q<CR>', opts)
