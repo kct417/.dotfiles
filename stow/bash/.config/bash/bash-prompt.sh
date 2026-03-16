b8830676690d292a0a18a472e0642f573ecfe7b5
Casey Tran <ctran156@calpoly.edu>
2026-03-16 10:27:40 +0000

    Configure PS2

--- stow/bash/.config/bash/bash-prompt.sh
+++ stow/bash/.config/bash/bash-prompt.sh
@@ -1,7 +1,8 @@
 source ~/.config/bash/git-prompt.sh
 
 PS1_CMD1="\$(__git_ps1 \" (%s)\")"
-PS1="\[\e[96m\][\T]\[\e[92m\][\u@\h]\[\e[0m\] \[\e[94m\]\W\[\e[37m\]${PS1_CMD1}\n\[\e[0m\]❯\[\e]0;\w\a\] "
+PS1="\[\e[96m\][\[\e[96m\]\T\[\e[96m\]]\[\e[92m\][\[\e[92m\]\u\[\e[92m\]@\[\e[92m\]\h\[\e[92m\]]\[\e[0m\] \[\e[94m\]\W\[\e[37m\]${PS1_CMD1}\n\[\e[95m\]❯\[\e]0;\w\a\]\[\e[0m\] "
+PS2="\[\e[95m\]❯\[\e[0m\] "
 
 function lastcommand {
     history | tail -1 | cut -c 8-
@@ -14,5 +15,5 @@ function deleteprompt {
     tput ed
 }
 
-PS0='\[$(deleteprompt)\]❯ $(lastcommand)\n\[${PS1:0:$((EXPS0=1,0))}\]'
+PS0='\[$(deleteprompt)\]\[\e[95m\]❯\[\e[0m\] $(lastcommand)\n\[${PS1:0:$((EXPS0=1,0))}\]'
 PROMPT_COMMAND='[ "$EXPS0" = 0 ] && deleteprompt && echo -e "❯" || EXPS0=0'
