6a51e76fcc8beb1720f2b6bc4d1bedccd716622c
Casey Tran <ctran156@calpoly.edu>
2026-03-18 20:47:12 +0000

    Reconfigure bash scripts

--- bin/bash/init.sh
+++ bin/bash/init.sh
@@ -1,16 +1,4 @@
 echo "--- bash/init.sh ---"
 BASHRC=stow/bash/.bashrc
-LOCAL=PATH=\"\$HOME/.local/bin:\$PATH\"
-
-BASH=.config/bash
-GIT=.config/git
-ALIAS="if [ -f ~/$BASH/.bash_aliases ]; then . ~/$BASH/.bash_aliases; fi"
-PROMPT="if [ -f ~/$BASH/bash-prompt.sh ]; then . ~/$BASH/bash-prompt.sh; fi"
-SSH="if [ -f ~/$GIT/ssh-agent.sh ]; then . ~/$GIT/ssh-agent.sh; fi"
-ZOXIDE="if command -v zoxide >/dev/null 2>&1; then eval \"\$(zoxide init bash --cmd cd)\"; fi"
-
-if ! grep -qxF "$LOCAL" $BASHRC; then echo "$LOCAL" >> $BASHRC; echo "$LOCAL"; fi
-if ! grep -qxF "$ALIAS" $BASHRC; then echo "$ALIAS" >> $BASHRC; echo "$ALIAS"; fi
-if ! grep -qxF "$SSH" $BASHRC; then echo "$SSH" >> $BASHRC; echo "$SSH"; fi
-if ! grep -qxF "$PROMPT" $BASHRC; then echo "$PROMPT" >> $BASHRC; echo "$PROMPT"; fi
-if ! grep -qxF "$ZOXIDE" $BASHRC; then echo "$ZOXIDE" >> $BASHRC; echo "$ZOXIDE"; fi
+CONFIG="if [ -f ~/.config/bash/.bashconfig ]; then . ~/.config/bash/.bashconfig; fi"
+if ! grep -qxF "$CONFIG" $BASHRC; then echo "$CONFIG" >> $BASHRC; echo "$CONFIG"; fi
--- bin/bash/install.sh
+++ bin/bash/install.sh
@@ -1,13 +1,12 @@
 echo "--- bash/install.sh ---"
 DEFAULT=/etc/skel/.bashrc
-BASH=stow/bash
 BASHRC=stow/bash/.bashrc
-if [ ! $BASHRC ] then
-	mkdir -p $BASH
+if [ ! -f $BASHRC ]; then
 	if [ -f ~/.bashrc ]; then
 		cp ~/.bashrc $BASHRC
 		echo $BASHRC
 	else
 		cp $DEFAULT $BASHRC
 		echo $BASHRC
+	fi
 fi 
