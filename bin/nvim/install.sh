a81f8f5c2bcb495c8a0157c68949eabc2957bd60
Casey Tran <caseytran4@gmail.com>
2026-03-23 11:23:07 -0700

    Fix nvim install script

--- bin/nvim/install.sh
+++ bin/nvim/install.sh
@@ -3,5 +3,5 @@ if ! command -v nvim >/dev/null 2>&1; then
 	URL=https://github.com/neovim/neovim/releases/latest/download
 	if [[ $(uname -m) == "aarch64" ]]; then BASENAME=nvim-linux-arm64; else BASENAME=nvim-linux-x86_64; fi
 	mkdir -p ~/.local
-	curl -L $BASENAME.tar.gz $URL/$BASENAME.tar.gz | tar -xzf - -C ~/.local --strip-components=1
+	curl -L $URL/$BASENAME.tar.gz | tar -xzf - -C ~/.local --strip-components=1
 fi
