04f81dcbccebaa10f72f53c50ae61f8db0b92607
Casey Tran <caseytran4@gmail.com>
2026-03-30 09:36:00 +0000

    Add verbose install to blesh

--- bin/config/bash.sh
+++ bin/config/bash.sh
@@ -2,9 +2,9 @@ if [ "$1" = "install" ]; then
 	echo "--- bash/install.sh ---"
 
 	if [ ! -f ~/.local/share/blesh/ble.sh ]; then
-		curl -L https://github.com/akinomyoga/ble.sh/releases/download/nightly/ble-nightly.tar.xz | tar xJf -
+		curl -vL https://github.com/akinomyoga/ble.sh/releases/download/nightly/ble-nightly.tar.xz | tar xJf -
 		bash ble-nightly/ble.sh --install ~/.local/share
-		rm -rv ble-nightly
+		rm -vr ble-nightly
 	fi
 
 elif [ "$1" = 'init' ]; then
